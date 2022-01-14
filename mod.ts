63bb8645eaba02c02d1276eb6026aab2feb49dfb
iugo <iugogogo@gmail.com>
2022-01-14 13:22:01 +0800

    feat: fn getRunData

--- mod.ts
+++ mod.ts
@@ -36,3 +36,36 @@ export function loadENV(path = './.env'): void {
     console.warn('loadENV', err);
   }
 }
+
+/**
+ * [Deno] 执行命令
+ * @param command 需要执行的命令
+ * @returns 命令执行结果
+ */
+ export async function getRunData(command: string[]) {
+  /** 进程 */
+  const run = Deno.run({
+    cmd: command,
+    stdout: 'piped',
+    stderr: 'piped',
+  });
+
+  try {
+    /** 状态 */
+    const s1 = await run.status();
+
+    /** 信息 */
+    const stderr = await run.stderrOutput();
+    if (!s1.success) {
+      const r1err = new TextDecoder().decode(stderr);
+      throw new Error(r1err);
+    }
+
+    const res = new TextDecoder().decode(await run.output());
+    console.log('run', res);
+
+    return res;
+  } finally {
+    run.close();
+  }
+}
