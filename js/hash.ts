6809f7e70d54bc942d3fe246d0e5e41888e8d089
iugo <iugogogo@gmail.com>
2022-06-10 17:34:57 +0800

    feat: add fn hmac

--- js/hash.ts
+++ js/hash.ts
@@ -20,3 +20,32 @@ export function hexString(data: Uint8Array): string {
   }
   return s;
 }
+
+export async function hmac(
+  hash: 'SHA-256' | 'SHA-512',
+  /** secret key */
+  s: string | Uint8Array,
+  /** data */
+  d: string | Uint8Array,
+) {
+  const keyData = typeof s === 'string' ? new TextEncoder().encode(s) : s;
+  const key = await crypto.subtle.importKey(
+    'raw',
+    keyData,
+    { name: 'HMAC', hash },
+    true,
+    ['sign', 'verify'],
+  );
+
+  const data = typeof d === 'string' ? new TextEncoder().encode(d) : d;
+
+  const res = new Uint8Array(
+    await crypto.subtle.sign(
+      'HMAC',
+      key,
+      data,
+    ),
+  );
+
+  return res;
+}
