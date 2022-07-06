a5217f0f84d3b03987f664a3b56933be7704208f
iugo <iugogogo@gmail.com>
2022-07-06 09:47:05 +0800

    feat: add network error fn

---
+++ js/err.ts
@@ -0,0 +1,16 @@
+/**
+ * 判断一个错误是否是网络错误, 如果是网络错误, 一般可以重试
+ * @param err 需要判断的错误, 需要继承 `Error`
+ * @returns 返回是否是网络错误
+ */
+export function isNetworkError(err: Error): boolean {
+  if (
+    err.name === 'ConnectionError' ||
+    err.name === 'ConnectionReset' ||
+    err.name === 'TimedOut' ||
+    err.message.includes('Could not check if server accepts SSL connections')
+  ) {
+    return true;
+  }
+  return false;
+}
