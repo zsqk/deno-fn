3ba0b1ccb350b6d95725fc9dfbb190ddff257112
iugo <iugogogo@gmail.com>
2022-07-11 18:32:22 +0800

    feat: fn copyMapSync

---
+++ deno/copy.ts
@@ -0,0 +1,44 @@
+import { ensureDirSync } from 'https://deno.land/std@0.147.0/fs/mod.ts';
+
+/**
+ * 文本文件简易复制
+ */
+export function copyMapSync(
+  /** 原文件夹地址, 以 `/` 结尾 */
+  src: string,
+  /** 新文件夹地址, 以 `/` 结尾 */
+  dest: string,
+  {
+    fn,
+    filter,
+  }: {
+    /** map 函数 */
+    fn: (c: string, name: string, dir: string) => string | null;
+    /** 过滤函数 */
+    filter?: (name: string, dir: string) => boolean;
+  },
+) {
+  function map(path: string) {
+    for (const dirEntry of Deno.readDirSync(src + path)) {
+      if (dirEntry.isDirectory) {
+        ensureDirSync(dest + path + dirEntry.name);
+        map(path + dirEntry.name + '/');
+      }
+      if (dirEntry.isFile) {
+        if (filter && !filter(dirEntry.name, path)) {
+          continue;
+        }
+        const c = fn(
+          Deno.readTextFileSync(src + path + dirEntry.name),
+          dirEntry.name,
+          path,
+        );
+        if (c === null) {
+          continue;
+        }
+        Deno.writeTextFileSync(dest + path + dirEntry.name, c);
+      }
+    }
+  }
+  map('');
+}
