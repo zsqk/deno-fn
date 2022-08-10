e7163620f2b34b778cb7afd13d6fa0ac729705be
iugo <iugogogo@gmail.com>
2022-08-10 19:23:54 +0800

    feat: genSafeString, genRandomString

--- js/str.test.ts
+++ js/str.test.ts
@@ -1,5 +1,6 @@
-import { isSafeString } from './str.ts';
+import { genRandomString, genSafeString, isSafeString } from './str.ts';
 import { assert } from 'https://deno.land/std@0.131.0/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@0.151.0/testing/asserts.ts';
 
 Deno.test('isSafeString', () => {
   assert(isSafeString('9'));
@@ -14,4 +15,28 @@ Deno.test('isSafeString', () => {
     !isSafeString(`
 `),
   );
+});
+
+Deno.test('genSafeString', () => {
+  const len = 65536;
+
+  console.time('genSafeString');
+  genSafeString(len);
+  console.timeEnd('genSafeString');
+
+  assertEquals(genSafeString(5).length, 5);
+  assertEquals(typeof genSafeString(5)[4], 'string');
+  console.log('genSafeString(5)', genSafeString(5));
+});
+
+Deno.test('genRandomString', () => {
+  const len = 65536;
+
+  console.time('genRandomString');
+  genRandomString(len);
+  console.timeEnd('genRandomString');
+
+  assertEquals(genRandomString(5).length, 5);
+  assertEquals(typeof genRandomString(5)[4], 'string');
+  console.log('genRandomString(5)', genRandomString(5));
 });
