f3348486adad87f635933dd3d0e52a079eb01d4b
iugo <iugogogo@gmail.com>
2022-09-01 11:49:55 +0800

    feat: autoRetry fn

--- js/err.ts
+++ js/err.ts
@@ -17,3 +17,42 @@ export function isNetworkError(err: Error): boolean {
   }
   return false;
 }
+
+/**
+ * [高阶函数] 网络错误时自动重试
+ * @param fn 函数本身, 需要为异步
+ * @param opt 参数
+ */
+export function autoRetry<T extends Array<any>, R>(
+  fn: (...rest: T) => Promise<R>,
+  { retry, isRetryable = isNetworkError }: {
+    /** 可重试次数 */
+    retry: number;
+    /** 判断发生错误后是否可以重试 */
+    isRetryable?: (err: Error) => boolean;
+  },
+) {
+  let left = retry;
+  let timeoutError: Error;
+  return async (...rest: T) => {
+    while (left > 0) {
+      left--;
+      console.log(`第 ${retry - left} 次尝试`);
+      try {
+        const res = await fn(...rest);
+        return res;
+      } catch (err: unknown) {
+        if (!(err instanceof Error)) {
+          throw new Error(`${err}`);
+        }
+        if (isRetryable(err)) {
+          timeoutError = err;
+          continue;
+        }
+        throw err;
+      }
+    }
+    timeoutError.message += ` and retryed ${retry} times`;
+    throw timeoutError;
+  };
+}
