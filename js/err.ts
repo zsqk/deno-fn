af29bfb517f289d206f6de8f2b50345ba7250cd7
iugo <iugogogo@gmail.com>
2022-09-01 12:17:17 +0800

    feat: autoRetry 新增延迟时间, 超时时间功能

--- js/err.ts
+++ js/err.ts
@@ -1,3 +1,5 @@
+import { delay } from 'https://deno.land/std@0.153.0/async/delay.ts';
+
 /**
  * 判断一个错误是否是网络错误, 如果是网络错误, 一般可以重试
  * @param err 需要判断的错误, 需要继承 `Error`
@@ -26,17 +28,25 @@ export function isNetworkError(err: Error): boolean {
 // deno-lint-ignore no-explicit-any
 export function autoRetry<T extends Array<any>, R>(
   fn: (...rest: T) => Promise<R>,
-  { retry = 3, isRetryable = isNetworkError }: {
+  { retry = 3, isRetryable = isNetworkError, delayms = 0, timeout = 0 }: {
     /** 可重试次数, 默认为 3 */
     retry?: number;
     /** 判断发生错误后是否可以重试 */
     isRetryable?: (err: Error) => boolean;
+    /** 延迟时间, 毫秒, 默认不延迟 */
+    delayms?: number;
+    /** 超时时间, 毫秒, 默认不限 */
+    timeout?: number;
   } = {},
 ) {
   let left = retry;
   let timeoutError: Error;
+  const max = Date.now() + timeout;
   return async (...rest: T) => {
     while (left > 0) {
+      if (timeout && Date.now() > max) {
+        break;
+      }
       left--;
       console.log(`第 ${retry - left} 次尝试`);
       try {
@@ -48,12 +58,13 @@ export function autoRetry<T extends Array<any>, R>(
         }
         if (isRetryable(err)) {
           timeoutError = err;
+          delayms && await delay(delayms);
           continue;
         }
         throw err;
       }
     }
-    timeoutError.message += ` and retryed ${retry} times`;
+    timeoutError.message += ` and retryed ${retry - left} times`;
     throw timeoutError;
   };
 }
