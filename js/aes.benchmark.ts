53abc1106c7f83251ee55f255669fd3615e8c595
iugo <iugogogo@gmail.com>
2022-09-29 10:33:16 +0800

    chore: 移除密钥生成基准测试

--- js/aes.benchmark.ts
+++ js/aes.benchmark.ts
@@ -1,11 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.131.0/testing/asserts.ts';
-import {
-  decrypt,
-  decryptBase,
-  encrypt,
-  encryptBase,
-  genAesKey,
-} from './aes.ts';
+import { genIV } from './aes.ts';
 
 Deno.test('genIV-1', () => {
   const times = 100000;
@@ -13,52 +6,13 @@ Deno.test('genIV-1', () => {
 
   console.time('Math.random');
   for (let i = 0; i < times; i++) {
-    new Uint8Array(l).map(() => Math.trunc(256 * Math.random()));
+    genIV(l, 'Math.random');
   }
   console.timeEnd('Math.random');
 
   console.time('crypto.getRandomValues');
   for (let i = 0; i < times; i++) {
-    window.crypto.getRandomValues(new Uint8Array(l));
+    genIV(l, 'getRandomValues');
   }
   console.timeEnd('crypto.getRandomValues');
-});
-
-Deno.test('genKey-1', async () => {
-  const times = 1000;
-  const content = '中文';
-
-  {
-    console.time('genkey');
-    for (let i = 0; i < times; i++) {
-      const d1 = await encrypt(
-        'aaabWe22zMdfpVVF6kFsZ9E4ODL1wjjykB5ifzjLzzz',
-        content,
-      );
-      const d2 = await decrypt(
-        'aaabWe22zMdfpVVF6kFsZ9E4ODL1wjjykB5ifzjLzzz',
-        d1,
-      );
-      assertEquals(d2, content);
-    }
-    console.timeEnd('genkey');
-  }
-
-  {
-    console.time('oncekey');
-    const [k] = await genAesKey(
-      'AES-CBC',
-    );
-    const iv = new Uint8Array(16).map(() => Math.trunc(256 * Math.random()));
-    for (let i = 0; i < times; i++) {
-      const d1 = await encryptBase(
-        k,
-        iv,
-        content,
-      );
-      const d2 = await decryptBase(k, iv, d1);
-      assertEquals(d2, content);
-    }
-    console.timeEnd('oncekey');
-  }
 });
