fc34e9f9dc9efd48698c1f8ae8b306623ffd0dd2
iugo <iugogogo@gmail.com>
2022-10-21 16:40:26 +0800

    feat: os parser

--- js/ua.test.ts
+++ js/ua.test.ts
@@ -1,4 +1,5 @@
 import {
+  assertEquals,
   assertObjectMatch,
 } from 'https://deno.land/std@0.131.0/testing/asserts.ts';
 
@@ -8,6 +9,14 @@ const iPadUA =
   `Mozilla/5.0 (iPad; CPU OS 15_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/19H12 AliApp(DingTalk/6.5.45) com.laiwang.DingTalk/26191496 Channel/201200 Pad/iPad language/zh-Hans-CN UT4Aplus/0.0.6 WK`;
 const androidUA =
   `Mozilla/5.0 (Linux; U; Android 10; zh-CN; EVR-AL00 Build/HUAWEIEVR-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.100 UWS/3.22.1.210 Mobile Safari/537.36 AliApp(DingTalk/6.5.45) com.alibaba.android.rimet/26284409 Channel/227200 language/zh-CN abi/64 Hmos/1 UT4Aplus/0.2.25 colorScheme/light`;
+const macosUA =
+  `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36`;
+const iPhoneUA =
+  `Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1`;
+const windowsUA =
+  `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36`;
+const crUA =
+  `Mozilla/5.0 (X11; CrOS x86_64 14816.131.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36`;
 
 Deno.test('ua-dingtalk', () => {
   const iPadRes = parserUA(iPadUA);
@@ -22,10 +31,32 @@ Deno.test('ua-dingtalk', () => {
   });
 });
 
+Deno.test('ua-os', () => {
+  {
+    const res = parserUA(macosUA);
+    assertObjectMatch(res, { os: 'macOS' });
+  }
+  {
+    const res = parserUA(iPhoneUA);
+    assertObjectMatch(res, { os: 'iOS' });
+  }
+  {
+    const res = parserUA(windowsUA);
+    assertObjectMatch(res, { os: 'Windows' });
+  }
+
+  {
+    const res = parserUA(crUA);
+    assertObjectMatch(res, { os: 'Chrome OS' });
+  }
+});
+
 Deno.test('debug-parserUACommon', () => {
   const res1 = parserUACommon(iPadUA);
+  assertEquals(res1.length, 10);
   console.log('res1', res1);
 
   const res2 = parserUACommon(androidUA);
+  assertEquals(res2.length, 15);
   console.log('res2', res2);
 });
--- js/ua.ts
+++ js/ua.ts
@@ -1,37 +1,84 @@
+type OSName =
+  | 'macOS'
+  | 'Windows'
+  | 'Android'
+  | 'HarmonyOS'
+  | 'iOS'
+  | 'iPadOS'
+  | 'Chrome OS'
+  | 'unknown';
+
 export function parserUA(ua: string): {
   isDingtalk: boolean;
   dingtalkVersion: string | null;
-  isHmos: boolean;
   hmosVersion: string | null;
+  isMobile: boolean;
+  os: OSName;
 } {
   const base = Object.fromEntries(
     parserUACommon(ua).map(({ key, ...rest }) => [key, rest]),
   );
+
+  let os: OSName = 'unknown';
+
   let isDingtalk = false;
   let dingtalkVersion: string | null = null;
-  {
-    const aliapp = base['AliApp'];
-    if (aliapp && aliapp.info.length === 1) {
-      // 根据真实 UA 猜测, 如果阿里将来 UA 变动, 可能导致出错
-      const [{ key, version }] = parserUACommon(aliapp.info[0]);
-      if (key === 'DingTalk') {
-        isDingtalk = true;
-        dingtalkVersion = version!;
-      }
+  const aliapp = base['AliApp'];
+  if (aliapp && aliapp.info.length === 1) {
+    // 根据真实 UA 猜测, 如果阿里将来 UA 变动, 可能导致出错
+    const [{ key, version }] = parserUACommon(aliapp.info[0]);
+    if (key === 'DingTalk') {
+      isDingtalk = true;
+      dingtalkVersion = version!;
     }
   }
 
-  let isHmos = false;
+  // 简单判断 OS 信息
+  for (const info of base['Mozilla'].info) {
+    // TODO: 从 13 开始, 改名为 iPadOS
+    if (info === 'iPad') {
+      os = 'iPadOS';
+      break;
+    }
+    if (info === 'iPhone') {
+      os = 'iOS';
+      break;
+    }
+    if (info === 'Macintosh') {
+      os = 'macOS';
+      break;
+    }
+    if (info.startsWith('Windows NT')) {
+      os = 'Windows';
+      break;
+    }
+    if (info.startsWith('Android')) {
+      os = 'Android';
+      break;
+    }
+    if (info.startsWith('CrOS')) {
+      os = 'Chrome OS';
+      break;
+    }
+  }
+
+  // TODO: 判断是否为 Android
+  //       (注意, 一定要在判断 hmos 之前, 否则会因兼容导致误判)
+
   let hmosVersion: string | null = null;
-  {
-    const hmos = base['Hmos'];
-    if (hmos) {
-      isHmos = true;
-      hmosVersion = hmos.version!;
-    }
+  const hmos = base['Hmos'];
+  if (hmos) {
+    os = 'HarmonyOS';
+    hmosVersion = hmos.version!;
   }
 
-  return { isDingtalk, dingtalkVersion, isHmos, hmosVersion };
+  return {
+    isDingtalk,
+    dingtalkVersion,
+    hmosVersion,
+    isMobile: base['Mobile'] !== undefined,
+    os,
+  };
 }
 
 export function parserUACommon(
