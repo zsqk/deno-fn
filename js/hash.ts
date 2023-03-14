97560ce5eedba682d39b2d8fc87fba41b86baf56
iugo <iugogogo@gmail.com>
2023-03-14 15:35:53 +0800

    feat: RSA sign

--- js/hash.ts
+++ js/hash.ts
@@ -1,4 +1,5 @@
 import { encode } from 'https://deno.land/std@0.143.0/encoding/hex.ts';
+import { decode } from 'https://deno.land/std@0.179.0/encoding/base64.ts';
 
 export async function hashString(
   algorithm: 'SHA-1' | 'SHA-256' | 'SHA-512',
@@ -58,6 +59,51 @@ export async function hmac(
   const res = new Uint8Array(
     await crypto.subtle.sign(
       'HMAC',
+      cryptoKey,
+      data,
+    ),
+  );
+
+  return res;
+}
+
+/**
+ * Sign data with RSA
+ * - only pkcs8
+ * - only RSASSA-PKCS1-v1_5
+ * @param key
+ * @param d
+ * @returns
+ */
+export async function rasSign(
+  key: {
+    hash: 'SHA-256' | 'SHA-512';
+    /** secret key */
+    s: string | Uint8Array;
+  } | CryptoKey,
+  /** data */
+  d: string | Uint8Array,
+): Promise<Uint8Array> {
+  let cryptoKey: CryptoKey;
+  if (key instanceof CryptoKey) {
+    cryptoKey = key;
+  } else {
+    const { s, hash } = key;
+    const keyData = typeof s === 'string' ? decode(s) : s;
+    cryptoKey = await crypto.subtle.importKey(
+      'pkcs8',
+      keyData,
+      { name: 'RSASSA-PKCS1-v1_5', hash: { name: hash } },
+      true,
+      ['sign'],
+    );
+  }
+
+  const data = typeof d === 'string' ? new TextEncoder().encode(d) : d;
+
+  const res = new Uint8Array(
+    await crypto.subtle.sign(
+      'RSASSA-PKCS1-v1_5',
       cryptoKey,
       data,
     ),
