2c48ccceadeccb6029aa89dda45943cbbba40cb6
iugo <iugogogo@gmail.com>
2023-07-07 10:36:59 +0800

    feat: fn textWithBOM

--- js/str.ts
+++ js/str.ts
@@ -90,3 +90,28 @@ export function fromUnicodeStr(str: string): string {
   }
   return encoded;
 }
+
+/**
+ * 为文本添加 UTF-8 BOM (for Windows...)
+ * - [Magic Number](https://en.wikipedia.org/wiki/Magic_number_(programming))
+ * - [Byte Order Mark](https://en.wikipedia.org/wiki/Byte_order_mark)
+ * @param v 需要增加 BOM 的文本
+ * @returns
+ */
+export function textWithBOM(v: Uint8Array | string): Uint8Array {
+  let data: Uint8Array;
+  if (typeof v === 'string') {
+    const encoder = new TextEncoder();
+    data = encoder.encode(v);
+  } else {
+    data = v;
+  }
+
+  /** EF BB BF */
+  const mn = [239, 187, 191];
+  const u8a = new Uint8Array(data.length + mn.length);
+  u8a.set(mn);
+  u8a.set(data, mn.length);
+
+  return u8a;
+}
