a1ed2cba3f70315a910440ef4b17487388897923
iugo <iugogogo@gmail.com>
2024-05-23 11:05:04 +0800

    feat: [类型] 函数返回结果

--- ts/error.ts
+++ ts/error.ts
@@ -8,3 +8,12 @@ export function assertError(v: unknown): asserts v is Error {
     throw new TypeError(`should be Error but ${v}`);
   }
 }
+
+/**
+ * [类型] 函数返回结果
+ *
+ * @description 可能出现错误的结果 (类似 Rust Result)
+ */
+export type Result<T, E extends Error = Error, L extends boolean = true> =
+  L extends true ? [E, null] | [true, null] | [null, T]
+    : [E, null] | [null, T];
