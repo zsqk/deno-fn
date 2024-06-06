9c2c903247218960a28674823e6a66a284e6c606
iugo <iugogogo@gmail.com>
2024-06-06 17:58:40 +0800

    test: safeWarpSync and safeWarp

--- js/err.test.ts
+++ js/err.test.ts
@@ -1,6 +1,6 @@
 import { assertIsError } from 'https://deno.land/std@0.217.0/assert/assert_is_error.ts';
 import { delay } from 'https://deno.land/std@0.217.0/async/delay.ts';
-import { autoRetry } from './err.ts';
+import { autoRetry, safeWarp, safeWarpSync } from './err.ts';
 
 Deno.test('autoRetry', async () => {
   async function test(time: number) {
@@ -16,4 +16,16 @@ Deno.test('autoRetry', async () => {
   await fn(1).catch((err) => {
     assertIsError(err, Error, 'retryed');
   });
+});
+
+Deno.test('safeWarpSync and safeWarp', () => {
+  const nf1 = safeWarpSync((name: string) => 1, () => {});
+  const nf2 = safeWarpSync((name: string) => 1, () => 2);
+  // 👇 预期类型错误
+  // const nf3 = safeWarpSync((name: string) => Promise.resolve(1), () => 2);
+
+  const anf1 = safeWarp(async (name?: string) => 1, () => {});
+  const anf2 = safeWarp(async (name: string) => 1, () => 2);
+  // 👇 预期类型错误
+  // const anf3 = safeWarp((name: string) => 1, () => 2);
 });
