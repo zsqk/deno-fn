f491424fa6986e7ff3c96cc8fc23df78e6506e6b
iugo <iugogogo@gmail.com>
2025-04-02 16:46:00 +0800

    feat: 增强GBK编码功能
    - 在`encodeToGbk`函数中添加了详细的文档注释，说明参数和返回值。
    - 优化了GBK编码逻辑，修复了对空格字符的处理方式。
    - 增加了对非中文字符的处理逻辑，确保编码的准确性。

--- js/gbk.ts
+++ js/gbk.ts
@@ -1,6 +1,13 @@
 import gbkMapJSON from './gbk-map.json' with { type: 'json' };
 const gbkMap: Record<string, number> = gbkMapJSON;
 
+/**
+ * 将字符串编码为 GBK 编码
+ * @param src 字符串
+ * @returns GBK 编码
+ *
+ * @author iugo <code@iugo.dev>
+ */
 export function encodeToGbk(src: string): string {
   let res = '';
   for (let D = 0; D < src.length; D++) {
@@ -10,12 +17,16 @@ export function encodeToGbk(src: string): string {
       throw Error('can not encode to gbk, not support');
     }
     /**
-     * GBK 编码位点
+     * GBK 码点
      */
     let e = gbkMap[charCode.toString()]?.toString(16).toUpperCase();
-    if ((e && e.length % 2 !== 0 && (e = '0' + e), ' ' === char)) {
+    if (e && e.length % 2 !== 0) {
+      e = '0' + e;
+    }
+    if (' ' === char) {
       res += '20';
     } else if (19968 > charCode) {
+      // 不是中文字符
       let t = charCode.toString(16);
       t && t.length % 2 !== 0 && (t = '0' + t), (res += t);
     } else {
