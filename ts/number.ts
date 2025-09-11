7a84011e544c28dd489cef3792c305fcec4360de
iugo <iugogogo@gmail.com>
2025-09-11 18:39:08 +0800

    feat: 增加正整数断言函数及优化判断逻辑
    - 新增`assertPositiveInteger`函数，用于断言传入值为正整数，支持可选参数以自定义错误处理和允许null/undefined。
    - 优化`isPositiveInteger`函数的判断逻辑，增强代码可读性。

--- ts/number.ts
+++ ts/number.ts
@@ -14,7 +14,42 @@ export type PositiveInteger = number;
 
 /** 判断是否为正整数 (不包含 0) */
 export function isPositiveInteger(v: unknown): v is PositiveInteger {
-  return Number.isSafeInteger(v) && (v as number) > 0;
+  if (!Number.isSafeInteger(v)) {
+    return false;
+  }
+  return (v as number) > 0;
+}
+
+/**
+ * 断言传入值是一个正整数 (不包含 0)
+ * @param v 传入值
+ * @param options 可选参数
+ * @param options.genErr 生成错误的方法, 默认是返回一个 TypeError
+ * @param options.allowNull 是否允许传入 null, 默认不允许
+ * @param options.allowUndefined 是否允许传入 undefined, 默认不允许
+ * @returns
+ */
+export function assertPositiveInteger(
+  v: unknown,
+  {
+    genErr = () => new TypeError(`should be positive integer but ${v}`),
+    allowNull,
+    allowUndefined,
+  }: {
+    genErr?: (v: unknown) => Error;
+    allowNull?: boolean;
+    allowUndefined?: boolean;
+  } = {},
+): asserts v is PositiveInteger {
+  if (allowNull && v === null) {
+    return;
+  }
+  if (allowUndefined && v === undefined) {
+    return;
+  }
+  if (!isPositiveInteger(v)) {
+    throw genErr(v);
+  }
 }
 
 /** 人民币分 */
