469617fb35bca3eebe9654d96236636e6088785a
iugo <iugogogo@gmail.com>
2025-09-11 18:50:42 +0800

    chore: 增强`assertPositiveInteger`函数，支持更多类型的断言
    - 更新`assertPositiveInteger`函数，允许传入`null`和`undefined`，并提供相应的选项以支持不同的断言类型。
    - 增加详细的文档注释，明确每种情况下的参数和返回值，提升代码可读性和灵活性。

--- ts/number.ts
+++ ts/number.ts
@@ -22,11 +22,12 @@ export function isPositiveInteger(v: unknown): v is PositiveInteger {
 
 /**
  * 断言传入值是一个正整数 (不包含 0)
+ * 默认情况下只允许正整数，不允许 null 或 undefined
  * @param v 传入值
  * @param options 可选参数
  * @param options.genErr 生成错误的方法, 默认是返回一个 TypeError
- * @param options.allow 额外允许的值类型
- * @returns
+ * @param options.allow 额外允许的值类型, 默认不允许
+ * @returns 断言 v 是 PositiveInteger 类型
  */
 export function assertPositiveInteger(
   v: unknown,
@@ -36,6 +37,15 @@ export function assertPositiveInteger(
   },
 ): asserts v is PositiveInteger;
 
+/**
+ * 断言传入值是一个正整数 (不包含 0) 或 null
+ * 允许传入 null 值，其他情况必须为正整数
+ * @param v 传入值
+ * @param options 可选参数
+ * @param options.genErr 生成错误的方法, 默认是返回一个 TypeError
+ * @param options.allow 必须为 'null'，表示允许 null 值
+ * @returns 断言 v 是 PositiveInteger | null 类型
+ */
 export function assertPositiveInteger(
   v: unknown,
   options: {
@@ -44,6 +54,15 @@ export function assertPositiveInteger(
   },
 ): asserts v is PositiveInteger | null;
 
+/**
+ * 断言传入值是一个正整数 (不包含 0) 或 undefined
+ * 允许传入 undefined 值，其他情况必须为正整数
+ * @param v 传入值
+ * @param options 可选参数
+ * @param options.genErr 生成错误的方法, 默认是返回一个 TypeError
+ * @param options.allow 必须为 'undefined'，表示允许 undefined 值
+ * @returns 断言 v 是 PositiveInteger | undefined 类型
+ */
 export function assertPositiveInteger(
   v: unknown,
   options: {
@@ -52,6 +71,15 @@ export function assertPositiveInteger(
   },
 ): asserts v is PositiveInteger | undefined;
 
+/**
+ * 断言传入值是一个正整数 (不包含 0)、null 或 undefined
+ * 允许传入 null 和 undefined 值，其他情况必须为正整数
+ * @param v 传入值
+ * @param options 可选参数
+ * @param options.genErr 生成错误的方法, 默认是返回一个 TypeError
+ * @param options.allow 必须为 'null-undefined'，表示允许 null 和 undefined 值
+ * @returns 断言 v 是 PositiveInteger | null | undefined 类型
+ */
 export function assertPositiveInteger(
   v: unknown,
   options: {
